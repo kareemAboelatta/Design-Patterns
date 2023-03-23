f7ad0036cfb21f89482fc5a0d14581403a19e67f
kareem_aboelatta <kareem.aboelatta@baianat.com>
2023-03-23 13:30:53 +0200

    Design Patterns: Observer Pattern (Arabic)

--- behavioral/Chain of Responsibility.kt
+++ behavioral/Chain of Responsibility.kt
@@ -48,16 +48,26 @@ class CEO : Handler(){
 }
 
 fun main() {
-    var radwa = Director()
-    var nadeen = VP()
-    var mostafa = CEO()
+    val radwa = Director()
+    val nadeen = VP()
+    val mostafa = CEO()
 
     radwa.setSuccessor(nadeen)
     nadeen.setSuccessor(mostafa)
 
 
-    var request = Request(RequestType.CONFERENCE,500.0)
+    val request = Request(RequestType.CONFERENCE,500.0)
     radwa.handleRequest(request)
 
 }
 
+
+/**
+ * PitFalls
+ *  - Efficiency issues with long chains
+ *  -No guarantee for request handling
+ *
+ * */
+
+//Behavioral Design Patterns: Chain of Responsibility (Arabic)
+// video link = https://www.youtube.com/watch?v=-UQND--CVew&list=PLd-dOEgzBpGnt3GuEszo_piQq52XSqAmj&index=1
---
+++ behavioral/Observer Pattern.kt
@@ -0,0 +1,118 @@
+package behavioral
+
+abstract class Subject{
+    private var  observers = ArrayList<Observer>()
+
+
+
+    abstract  fun setState(state:String)
+    abstract  fun getState(): String
+
+    fun attach(observer: Observer){
+        observers.add(observer)
+    }
+
+    fun detach(observer: Observer){
+        observers.remove(observer)
+    }
+
+    fun notifyAllObservers(){
+        observers.forEach {observer->
+            observer.update()
+        }
+    }
+}
+
+
+abstract class Observer{
+    lateinit var subject: Subject
+    abstract fun update()
+}
+
+
+
+class ElliFeha : Subject(){
+    private var state = "Silent"
+
+    override fun setState(state: String) {
+        this.state =state
+        notifyAllObservers()
+    }
+    override fun getState(): String {
+        return  state
+    }
+
+}
+
+
+
+class Player(subject: Subject, val name: String) : Observer(){
+
+    init {
+        this.subject = subject
+        subject.attach(this)
+    }
+
+    fun edrab(){
+        subject.setState("aaah!")
+    }
+
+    override fun update() {
+        println("$name heard ${subject.getState()}")
+    }
+
+
+}
+
+
+fun main() {
+    val youssef = ElliFeha()
+
+    val kareem  = Player(name = "kareem" , subject = youssef)
+    val nosa  = Player(name = "nosa" , subject = youssef)
+    val mohamed  = Player(name = "mohamed" , subject = youssef)
+
+    kareem.edrab()
+    Thread.sleep(5000)
+
+
+    nosa.edrab()
+    Thread.sleep(5000)
+
+
+    mohamed.edrab()
+    Thread.sleep(5000)
+
+
+
+
+
+}
+
+/**
+ * '''Kotlin'''
+ * PitFalls
+ *  -Observable doesn't know which observer update its state
+ *  -Large updates (take care in implementation)(state management )
+ *  - Difficult Debugging
+ *
+ * */
+
+//Design Patterns: Observer Pattern (Arabic)
+//https://www.youtube.com/watch?v=R-N7YWJSol8&list=PLd-dOEgzBpGnt3GuEszo_piQq52XSqAmj&index=2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
