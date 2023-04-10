ad02c25a40c13ebea8bb245ad7b3be30f6fb3b92
kareem_aboelatta <kareem.aboelatta@baianat.com>
2023-04-10 10:24:32 +0200

    Strategy Design Pattern.

--- behavioral/Strategy Pattern.kt
+++ behavioral/Strategy Pattern.kt
@@ -14,6 +14,7 @@ class MathOperation(private var strategy: MathStrategy) {
 // Strategy interface
 interface MathStrategy {
     fun doOperation(a: Int, b: Int): Int
+
 }
 
 // Concrete Strategy classes
